631dee6b018dce6e5a76d565bbf0532ed031d2bf
Mujtaba-Quadri <mujtabaq@hawaii.edu>
2022-10-22 16:05:14 -1000

    Revert "right-aligned login button, ugly but functional page"
    This reverts commit c51ba200d5259fe5c813ae70d491f797b3d05d29.

--- software-billders/pages/bills.tsx
+++ software-billders/pages/bills.tsx
@@ -10,11 +10,8 @@ import {
 } from "@firebase/firestore";
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { firestore } from '../firebase/firebaseClient'
-import styles from '../styles/Home.module.css'
-import SignIn from './login'
-import { Container } from 'semantic-ui-react';
 
-const Bills: NextPage = () => {
+const Home: NextPage = () => {
   // This will query for 10 documents, increase this by changing the limit()
   const [value, loading, error] = useCollection(
     query(
@@ -26,35 +23,23 @@ const Bills: NextPage = () => {
     });
 
   return (
-    <Container>
-        {SignIn()}
+    <div>
       <p>
         {error && <strong>Error: {JSON.stringify(error)}</strong>}
         {loading && <span>Collection: Loading...</span>}
         {value && (
-            <div>
-                <h1 className={styles.title}>Bills</h1>
-                <table>
-                    <tr>
-                        <th>Bills</th>
-                        <th>title</th>
-                        <th>Bills</th>
-                        <th>Bills{' '}</th>
-                        <th>Bills{' '}</th>
-                    </tr>
-                    {value.docs.map((doc) => (
-                    <tr key={doc.id}>
-                        <td>{doc.data().code}</td>
-                        <td>{doc.data().measureTitle}</td>
-                        <td>{new Date(doc.data().lastUpdated * 1000).toString()}</td>
-                    </tr>
-                    ))}
-                </table>
-            </div>
+          <span>
+            Bills:{' '}
+            {value.docs.map((doc) => (
+              <div key={doc.id}>
+                {doc.data().code},{doc.data().measureTitle},{new Date(doc.data().lastUpdated * 1000).toString()}
+              </div>
+            ))}
+          </span>
         )}
       </p>
-    </Container>
+    </div>
   )
 }
 
-export default Bills
+export default Home
